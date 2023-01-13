8fb8377d80d12290d61cf5d421c44d7a125bd5db
yogesh <yogesh@bhawsar.dev>
2023-01-13 19:16:38 +0530

    Add asyncStorage and change iOS bundle name

--- src/store/favouriteStore.ts
+++ src/store/favouriteStore.ts
@@ -1,27 +1,36 @@
 import {Story} from 'types/StoryTypes';
 import {create} from 'zustand';
+import {persist} from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type favouriteState = {
   favourites: Story[];
-  toggleFavourite: (story: Story) => void;
+  toggleFavourite: (story: Story) => Promise<void>;
 };
 
-const useFavouriteStore = create<favouriteState>((set, get) => ({
-  favourites: [],
-
-  toggleFavourite: (story: Story) => {
-    const {favourites} = get();
-    const isFavourite = favourites.find(fav => fav.id === story.id);
-    if (isFavourite) {
-      set({
-        favourites: favourites.filter(fav => fav.id !== story.id),
-      });
-    } else {
-      set({
-        favourites: [...favourites, story],
-      });
-    }
-  },
-}));
+const useFavouriteStore = create<favouriteState>()(
+  persist(
+    (set, get) => ({
+      favourites: [],
+      toggleFavourite: async (story: Story) => {
+        const {favourites} = get();
+        const isFavourite = favourites.find(fav => fav.id === story.id);
+        if (isFavourite) {
+          set({
+            favourites: favourites.filter(fav => fav.id !== story.id),
+          });
+        } else {
+          set({
+            favourites: [...favourites, story],
+          });
+        }
+      },
+    }),
+    {
+      name: 'auth',
+      getStorage: () => AsyncStorage,
+    },
+  ),
+);
 
 export default useFavouriteStore;
